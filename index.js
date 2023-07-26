34d09fdc8545d6ac20218c81e959d89272c4c5a3
Manith kumar <117072043+Manithkumar016@users.noreply.github.com>
2023-07-26 13:48:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Allow requests from localhost:3000
+    origin: 'https://chat-app-frontend-chi-pearl.vercel.app', // Allow requests from localhost:3000
     optionsSuccessStatus: 200, // Return 200 for successful preflight requests
     credentials: true,
   };
